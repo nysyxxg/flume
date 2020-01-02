0380fdc68a07546005e09ce99ba9b6c54881fbd2
mlamp <xuxiaoguang@mininglamp.com>
2020-01-02 15:50:39 +0800

    修改Httpsource代码

---
+++ flume-ng-core/src/main/java/org/apache/flume/source/my/HTTPSourceXMLHandler.java
@@ -0,0 +1,128 @@
+package org.apache.flume.source.my;
+
+import com.google.common.base.Preconditions;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.http.HTTPBadRequestException;
+import org.apache.flume.source.http.HTTPSourceHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * http source handler自定义例子
+ * 假定xml请求格式，期望格式如下：
+ *
+ * <events>
+ *  <event>
+ *      <headers><header1>value1</header1></headers>
+ *      <body>test</body>
+ *  </event>
+ *  <event>
+ *     <headers><header1>value1</header1></headers>
+ *     <body>test2</body>
+ *   </event>
+ *  </events>
+ * 现在要求flume http source可以处理这种请求的xml格式
+ */
+public class HTTPSourceXMLHandler implements HTTPSourceHandler {
+    private final String ROOT = "events";
+    private final String EVENT_TAG = "event";
+    private final String HEADERS_TAG = "headers";
+    private final String BODY_TAG = "body";
+ 
+    private final String CONF_INSERT_TIMESTAMP = "insertTimestamp";
+    private final String TIMESTAMP_HEADER = "timestamp";
+    private final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+ 
+    // Document builders are not thread-safe.
+    // So make sure we have one for each thread.
+    private final ThreadLocal<DocumentBuilder> docBuilder = new ThreadLocal<DocumentBuilder>();
+ 
+    private boolean insertTimestamp;
+    private static final Logger LOG = LoggerFactory.getLogger(HTTPSourceXMLHandler.class);
+ 
+ 
+    public List<Event> getEvents(HttpServletRequest httpServletRequest) throws HTTPBadRequestException, Exception {
+        if (docBuilder.get() == null) {
+            docBuilder.set(documentBuilderFactory.newDocumentBuilder());
+        }
+        Document doc;
+        final List<Event> events;
+        try {
+            doc = docBuilder.get().parse(httpServletRequest.getInputStream());            
+            Element root = doc.getDocumentElement();        
+ 
+            root.normalize();
+            // Verify that the root element is "events"
+            Preconditions.checkState(
+                    ROOT.equalsIgnoreCase(root.getTagName()));
+ 
+            NodeList nodes = root.getElementsByTagName(EVENT_TAG);
+            LOG.info("get nodes={}",nodes);
+ 
+            int eventCount = nodes.getLength();
+            events = new ArrayList<Event>(eventCount);
+            for (int i = 0; i < eventCount; i++) {
+                Element event = (Element) nodes.item(i);
+                // Get all headers. If there are multiple header sections,
+                // combine them.
+                NodeList headerNodes
+                        = event.getElementsByTagName(HEADERS_TAG);
+                Map<String, String> eventHeaders
+                        = new HashMap<String, String>();
+                for (int j = 0; j < headerNodes.getLength(); j++) {
+                    Node headerNode = headerNodes.item(j);
+                    NodeList headers = headerNode.getChildNodes();
+                    for (int k = 0; k < headers.getLength(); k++) {
+                        Node header = headers.item(k);
+                        // Read only element nodes
+                        if (header.getNodeType() != Node.ELEMENT_NODE) {
+                            continue;
+                        }
+                        // Make sure a header is inserted only once,
+                        // else the event is malformed
+                        Preconditions.checkState(
+                                !eventHeaders.containsKey(header.getNodeName()),
+                                "Header expected only once " + header.getNodeName());
+                        eventHeaders.put(
+                                header.getNodeName(), header.getTextContent());
+                    }
+                }
+                Node body = event.getElementsByTagName(BODY_TAG).item(0);
+                if (insertTimestamp) {
+                    eventHeaders.put(TIMESTAMP_HEADER, String.valueOf(System
+                            .currentTimeMillis()));
+                }
+                events.add(EventBuilder.withBody(
+                        body.getTextContent().getBytes(
+                                httpServletRequest.getCharacterEncoding()),
+                        eventHeaders));
+            }
+        } catch (SAXException ex) {
+            throw new HTTPBadRequestException(
+                    "Request could not be parsed into valid XML", ex);
+        } catch (Exception ex) {
+            throw new HTTPBadRequestException(
+                    "Request is not in expected format. " +
+                            "Please refer documentation for expected format.", ex);
+        }
+        return events;
+    }
+    public void configure(Context context) {
+        insertTimestamp = context.getBoolean(CONF_INSERT_TIMESTAMP, false);
+    }
+}
---
+++ flume-ng-core/src/main/java/org/apache/flume/source/my/MyHTTPSource.java
@@ -0,0 +1,308 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flume.source.my;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import org.apache.flume.ChannelException;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.instrumentation.SourceCounter;
+import org.apache.flume.source.AbstractSource;
+import org.apache.flume.source.http.HTTPBadRequestException;
+import org.apache.flume.source.http.HTTPSourceConfigurationConstants;
+import org.apache.flume.source.http.HTTPSourceHandler;
+import org.apache.flume.source.http.JSONHandler;
+import org.apache.flume.tools.FlumeBeanConfigurator;
+import org.apache.flume.tools.HTTPServerConstraintUtil;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.*;
+
+/**
+ * A source which accepts Flume Events by HTTP POST and GET. GET should be used
+ * for experimentation only. HTTP requests are converted into flume events by a
+ * pluggable "handler" which must implement the
+ * {@linkplain HTTPSourceHandler} interface. This handler takes a
+ * {@linkplain HttpServletRequest} and returns a list of flume events.
+ * <p>
+ * The source accepts the following parameters: <p> <tt>port</tt>: port to which
+ * the server should bind. Mandatory <p> <tt>handler</tt>: the class that
+ * deserializes a HttpServletRequest into a list of flume events. This class
+ * must implement HTTPSourceHandler. Default:
+ * {@linkplain JSONHandler}. <p> <tt>handler.*</tt> Any configuration
+ * to be passed to the handler. <p>
+ * <p>
+ * All events deserialized from one Http request are committed to the channel in
+ * one transaction, thus allowing for increased efficiency on channels like the
+ * file channel. If the handler throws an exception this source will return
+ * a HTTP status of 400. If the channel is full, or the source is unable to
+ * append events to the channel, the source will return a HTTP 503 - Temporarily
+ * unavailable status.
+ * <p>
+ * A JSON handler which converts JSON objects to Flume events is provided.
+ * 自己修改 HTTPSource： 修改flume源码，使其HTTPSource具备访问路径功能
+ * <p>
+ * MyHTTPSource：这个就是主类了，里面有类似于main方法的start方法。
+ * 其实本质上httpSource就是一个嵌入了jetty的服务器，通过接受post请求（目前写死了只处理post请求）。
+ * 将数据转换为event往下发。所以，修改很简单。
+ */
+public class MyHTTPSource extends AbstractSource implements
+        EventDrivenSource, Configurable {
+    /*
+     * There are 2 ways of doing this:
+     * a. Have a static server instance and use connectors in each source
+     *    which binds to the port defined for that source.
+     * b. Each source starts its own server instance, which binds to the source's
+     *    port.
+     *
+     * b is more efficient than a because Jetty does not allow binding a
+     * servlet to a connector. So each request will need to go through each
+     * each of the handlers/servlet till the correct one is found.
+     *
+     */
+
+    private static final Logger LOG = LoggerFactory.getLogger(MyHTTPSource.class);
+    private volatile Integer port;
+    private volatile Server srv;
+    private volatile String host;
+    private HTTPSourceHandler handler;
+    private SourceCounter sourceCounter;
+
+    // SSL configuration variable
+    private volatile String keyStorePath;
+    private volatile String keyStorePassword;
+    private volatile Boolean sslEnabled;
+    private final List<String> excludedProtocols = new LinkedList<String>();
+
+    private Context sourceContext;
+    private volatile String contextPath;
+
+    @Override
+    public void configure(Context context) {
+        sourceContext = context;
+        try {
+            contextPath = context.getString(HTTPSourceConfigurationConstants.CONTEXT_PATH);
+            // SSL related config
+            sslEnabled = context.getBoolean(HTTPSourceConfigurationConstants.SSL_ENABLED, false);
+
+            port = context.getInteger(HTTPSourceConfigurationConstants.CONFIG_PORT);
+            host = context.getString(HTTPSourceConfigurationConstants.CONFIG_BIND,
+                    HTTPSourceConfigurationConstants.DEFAULT_BIND);
+
+            Preconditions.checkState(host != null && !host.isEmpty(),
+                    "HTTPSource hostname specified is empty");
+            Preconditions.checkNotNull(port, "HTTPSource requires a port number to be"
+                    + " specified");
+
+            String handlerClassName = context.getString(
+                    HTTPSourceConfigurationConstants.CONFIG_HANDLER,
+                    HTTPSourceConfigurationConstants.DEFAULT_HANDLER).trim();
+
+            if (sslEnabled) {
+                LOG.debug("SSL configuration enabled");
+                keyStorePath = context.getString(HTTPSourceConfigurationConstants.SSL_KEYSTORE);
+                Preconditions.checkArgument(keyStorePath != null && !keyStorePath.isEmpty(),
+                        "Keystore is required for SSL Conifguration");
+                keyStorePassword =
+                        context.getString(HTTPSourceConfigurationConstants.SSL_KEYSTORE_PASSWORD);
+                Preconditions.checkArgument(keyStorePassword != null,
+                        "Keystore password is required for SSL Configuration");
+                String excludeProtocolsStr =
+                        context.getString(HTTPSourceConfigurationConstants.EXCLUDE_PROTOCOLS);
+                if (excludeProtocolsStr == null) {
+                    excludedProtocols.add("SSLv3");
+                } else {
+                    excludedProtocols.addAll(Arrays.asList(excludeProtocolsStr.split(" ")));
+                    if (!excludedProtocols.contains("SSLv3")) {
+                        excludedProtocols.add("SSLv3");
+                    }
+                }
+            }
+
+            @SuppressWarnings("unchecked")
+            Class<? extends HTTPSourceHandler> clazz = (Class<? extends HTTPSourceHandler>) Class.forName(handlerClassName);
+            handler = clazz.getDeclaredConstructor().newInstance();
+
+            Map<String, String> subProps = context.getSubProperties(HTTPSourceConfigurationConstants.CONFIG_HANDLER_PREFIX);
+            handler.configure(new Context(subProps));
+        } catch (ClassNotFoundException ex) {
+            LOG.error("Error while configuring HTTPSource. Exception follows.", ex);
+            Throwables.propagate(ex);
+        } catch (ClassCastException ex) {
+            LOG.error("Deserializer is not an instance of HTTPSourceHandler."
+                    + "Deserializer must implement HTTPSourceHandler.");
+            Throwables.propagate(ex);
+        } catch (Exception ex) {
+            LOG.error("Error configuring HTTPSource!", ex);
+            Throwables.propagate(ex);
+        }
+        if (sourceCounter == null) {
+            sourceCounter = new SourceCounter(getName());
+        }
+    }
+
+    @Override
+    public void start() {
+        Preconditions.checkState(srv == null,
+                "Running HTTP Server found in source: " + getName()
+                        + " before I started one."
+                        + "Will not attempt to start.");
+        QueuedThreadPool threadPool = new QueuedThreadPool();
+        if (sourceContext.getSubProperties("QueuedThreadPool.").size() > 0) {
+            FlumeBeanConfigurator.setConfigurationFields(threadPool, sourceContext);
+        }
+        srv = new Server(threadPool);
+
+        //Register with JMX for advanced monitoring
+        MBeanContainer mbContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
+        srv.addEventListener(mbContainer);
+        srv.addBean(mbContainer);
+
+        HttpConfiguration httpConfiguration = new HttpConfiguration();
+        httpConfiguration.addCustomizer(new SecureRequestCustomizer());
+
+        FlumeBeanConfigurator.setConfigurationFields(httpConfiguration, sourceContext);
+        ServerConnector connector;
+
+        if (sslEnabled) {
+            SslContextFactory sslCtxFactory = new SslContextFactory();
+            FlumeBeanConfigurator.setConfigurationFields(sslCtxFactory, sourceContext);
+            sslCtxFactory.setExcludeProtocols(excludedProtocols.toArray(new String[0]));
+            sslCtxFactory.setKeyStorePath(keyStorePath);
+            sslCtxFactory.setKeyStorePassword(keyStorePassword);
+
+            httpConfiguration.setSecurePort(port);
+            httpConfiguration.setSecureScheme("https");
+
+            connector = new ServerConnector(srv,
+                    new SslConnectionFactory(sslCtxFactory, HttpVersion.HTTP_1_1.asString()),
+                    new HttpConnectionFactory(httpConfiguration));
+        } else {
+            connector = new ServerConnector(srv, new HttpConnectionFactory(httpConfiguration));
+        }
+
+        connector.setPort(port);
+        connector.setHost(host);
+        connector.setReuseAddress(true);
+
+        FlumeBeanConfigurator.setConfigurationFields(connector, sourceContext);
+
+        srv.addConnector(connector);
+
+        try {
+            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            context.setContextPath("/");
+            srv.setHandler(context);
+
+            context.addServlet(new ServletHolder(new FlumeHTTPServlet()), "/"+ contextPath);
+            context.setSecurityHandler(HTTPServerConstraintUtil.enforceConstraints());
+            srv.start();
+        } catch (Exception ex) {
+            LOG.error("Error while starting HTTPSource. Exception follows.", ex);
+            Throwables.propagate(ex);
+        }
+        Preconditions.checkArgument(srv.isRunning());
+        sourceCounter.start();
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        try {
+            srv.stop();
+            srv.join();
+            srv = null;
+        } catch (Exception ex) {
+            LOG.error("Error while stopping HTTPSource. Exception follows.", ex);
+        }
+        sourceCounter.stop();
+        LOG.info("Http source {} stopped. Metrics: {}", getName(), sourceCounter);
+    }
+
+    private class FlumeHTTPServlet extends HttpServlet {
+
+        private static final long serialVersionUID = 4891924863218790344L;
+
+        @Override
+        public void doPost(HttpServletRequest request, HttpServletResponse response)
+                throws IOException {
+            List<Event> events = Collections.emptyList(); //create empty list
+            try {
+                events = handler.getEvents(request);
+            } catch (HTTPBadRequestException ex) {
+                LOG.warn("Received bad request from client. ", ex);
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "Bad request from client. "
+                                + ex.getMessage());
+                return;
+            } catch (Exception ex) {
+                LOG.warn("Deserializer threw unexpected exception. ", ex);
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "Deserializer threw unexpected exception. "
+                                + ex.getMessage());
+                return;
+            }
+            sourceCounter.incrementAppendBatchReceivedCount();
+            sourceCounter.addToEventReceivedCount(events.size());
+            try {
+                getChannelProcessor().processEventBatch(events);
+            } catch (ChannelException ex) {
+                LOG.warn("Error appending event to channel. "
+                        + "Channel might be full. Consider increasing the channel "
+                        + "capacity or make sure the sinks perform faster.", ex);
+                response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE,
+                        "Error appending event to channel. Channel might be full."
+                                + ex.getMessage());
+                return;
+            } catch (Exception ex) {
+                LOG.warn("Unexpected error appending event to channel. ", ex);
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "Unexpected error while appending event to channel. "
+                                + ex.getMessage());
+                return;
+            }
+            response.setCharacterEncoding(request.getCharacterEncoding());
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.flushBuffer();
+            sourceCounter.incrementAppendBatchAcceptedCount();
+            sourceCounter.addToEventAcceptedCount(events.size());
+        }
+
+        @Override
+        public void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws IOException {
+            doPost(request, response);
+        }
+    }
+}
